131f53ac51fe9dfbd7975a5e976987feab0b784e
钟杏彬 <pulangke_zhong@ikscrm.com>
2023-04-17 15:49:36 +0800

    temp

--- api/index.js
+++ api/index.js
@@ -1,12 +1,17 @@
-const http = require('http');
+const app = require('express')();
+const { v4 } = require('uuid');
 
-const server = http.createServer((req, resp) => {
+app.get('/api', (req, res) => {
+  const path = `/api/item/${v4()}`;
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
 
-    resp.end(JSON.stringify({
-        url: req.url,
-        method: req.method
-    }));
-})
+app.get('/api/item/:slug', (req, res) => {
+  const { slug } = req.params;
+  res.end(`Item: ${slug}`);
+});
 
-module.exports = server;
+module.exports = app;
 
